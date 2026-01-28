895a62a95344e3c14247770f276d1adc6daeb919
mridwanadityanp <mridwanadityanp@gmail.com>
2026-01-28 23:58:53 +0700

    cek

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+const routes = require('./routes/index.js')
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+app.use(routes)
+
+app.listen(PORT, () => {
+    console.log('Server is running on localhost://3000')
+})
+
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,1 +1,70 @@
-pp
+const { log } = require('console')
+// const {  } = require('../models/index')
+
+class Controller {
+    static async homePage(req,res){
+        try {
+
+            res.render('homePage')
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    }
+    static async loginPage(req,res){
+        try {
+
+            res.render('loginPage')
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    }
+    static async registerPage(req,res){
+        try {
+
+            res.render('registerPage')
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    }
+    static async homeProduct(req,res){
+        try {
+
+            res.render('homeProduct')
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    }
+    static async pageAddProduct(req,res){
+        try {
+
+            res.render('pageAddProduct')
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    }
+    static async submitAddProduct(req,res){
+        try {
+
+            res.redirect('/product')
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    }
+    static async paymentNotif(req,res){
+        try {
+
+            res.render('paymentNotif')
+        } catch (error) {
+            console.log(error);
+            res.send(error)
+        }
+    }
+}
+
+module.exports = Controller
