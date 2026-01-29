e1d35461925c25dd0dc182e708254961bd3a6c00
Teddy K <teddykoerniadi@gmail.com>
2026-01-29 12:16:16 +0700

    mantap jiwa

--- routes/author.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const authorRouter = express.Router()
-
-const Controller = require('../controllers/controller.js')
-
-authorRouter.get('/', Controller.authorPage)
-authorRouter.get('/detail', Controller.authorDetail)
-
-module.exports = authorRouter
